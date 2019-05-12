1f6777bb4886d447526104a5dc6cbdffa28d9853
daglus <daglus.ck.ua@gmail.com>
2019-05-12 15:32:39 +0300

    Add factory, add method and new spec to article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,3 +10,31 @@ describe Article do
     it { should have_many :comments }
   end
 end
+
+describe Article do
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Foo Bar')
+
+      expect(article.subject).to  eq 'Foo Bar'
+    end
+  end
+end
+
+describe Article do
+  describe "#last_comment" do
+    it "returns the last comment" do
+
+    end
+  end
+end
+
+describe Article do
+  describe "#last_comment" do
+    it "returns the last comment" do
+      article = create(:article_with_comments)
+
+      expect(article.last_comment.body).to eq "Comment body 3"
+    end
+  end
+end
