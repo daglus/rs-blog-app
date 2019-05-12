9e60f715d41abf51f64cb2badbcc7047ac2fe5ed
daglus <daglus.ck.ua@gmail.com>
2019-05-12 15:29:51 +0300

    Add gem "factory_bot_rails", add config

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
   gem 'capybara', '>= 2.15'
+  gem "factory_bot_rails"
 end
 
 group :development do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -59,10 +59,15 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  Shoulda::Matchers.configure do |config|
+Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
     with.library :rails
   end
 end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+end
+
 end
